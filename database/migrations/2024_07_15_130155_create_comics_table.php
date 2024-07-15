587f2e566ba6762841297d5f5e1d0a071d6469ef
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-15 15:20:25 +0200

    MILESTONE 1

---
+++ database/migrations/2024_07_15_130155_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 100);
+            $table->tinyInteger('price');
+            $table->string('series', 50);
+            $table->dateTime('sale_date');
+            $table->string('type', 30);
+            $table->string('artists', 30);
+            $table->string('writers', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
